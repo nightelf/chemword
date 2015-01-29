98b7030be7fcd4df50c2839e9611c2015b70fe22
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2015-01-28 22:46:26 -0800

    more elements.

--- chemword.js
+++ chemword.js
@@ -46,5 +46,227 @@ elements = {
         'symbol': 'O',
         'number': 8,
         'mass': 15.9994
+    },
+    'F': {
+        'name': 'Fluorine',
+        'symbol': 'F',
+        'number': 9,
+        'mass': 18.9984031636
+    },
+    'Ne': {
+        'name': 'Neon',
+        'symbol': 'F',
+        'number': 10,
+        'mass': 20.17976
+    },
+    'Na': {
+        'name': 'Sodium',
+        'symbol': 'Na',
+        'number': 11,
+        'mass': 22.989769282
+    },
+    'Mg': {
+        'name': 'Magnesium',
+        'symbol': 'Mg',
+        'number': 12,
+        'mass': 24.3051
+    },
+    'Al': {
+        'name': 'Aluminum',
+        'symbol': 'Al',
+        'number': 13,
+        'mass': 26.98153857
+    },
+    'Si': {
+        'name': 'Silicon',
+        'symbol': 'Si',
+        'number': 14,
+        'mass': 28.0851
+    },
+    'P': {
+        'name': 'Phosphorus',
+        'symbol': 'P',
+        'number': 15,
+        'mass': 30.9737619985
+    },
+    'S': {
+        'name': 'Sulfur',
+        'symbol': 'S',
+        'number': 16,
+        'mass': 32.061
+    },
+    'Cl': {
+        'name': 'Chlorine',
+        'symbol': 'Cl',
+        'number': 17,
+        'mass': 35.451
+    },
+    'Ar': {
+        'name': 'Argon',
+        'symbol': 'Ar',
+        'number': 18,
+        'mass': 39.9481
+    },
+    'K': {
+        'name': 'Potassium',
+        'symbol': 'Ar',
+        'number': 19,
+        'mass': 39.09831
+    },
+    'Ca': {
+        'name': 'Calcium',
+        'symbol': 'Ca',
+        'number': 20,
+        'mass': 40.0784
+    },
+    'Sc': {
+        'name': 'Scandium',
+        'symbol': 'Sc',
+        'number': 21,
+        'mass': 44.9559085
+    },
+    'Ti': {
+        'name': 'Titanium',
+        'symbol': 'Ti',
+        'number': 22,
+        'mass': 47.8671
+    },
+    'V': {
+        'name': 'Titanium',
+        'symbol': 'V',
+        'number': 23,
+        'mass': 50.94151
+    },
+    'Cr': {
+        'name': 'Chromium',
+        'symbol': 'Cr',
+        'number': 24,
+        'mass': 51.99616
+    },
+    'Mn': {
+        'name': 'Manganese',
+        'symbol': 'Mn',
+        'number': 25,
+        'mass': 54.9380443
+    },
+    'Fe': {
+        'name': 'Iron',
+        'symbol': 'Fe',
+        'number': 26,
+        'mass': 55.8452
+    },
+    'Co': {
+        'name': 'Cobalt',
+        'symbol': 'Co',
+        'number': 27,
+        'mass': 58.9331944
+    },
+    'Ni': {
+        'name': 'Nickel',
+        'symbol': 'Ni',
+        'number': 28,
+        'mass': 58.69344
+    },
+    'Cu': {
+        'name': 'Copper',
+        'symbol': 'Cu',
+        'number': 29,
+        'mass': 63.5463
+    },
+    'Zn': {
+        'name': 'Zinc',
+        'symbol': 'Zn',
+        'number': 30,
+        'mass': 65.382
+    },
+    'Ga': {
+        'name': 'Gallium',
+        'symbol': 'Ga',
+        'number': 31,
+        'mass': 69.7231
+    },
+    'Ge': {
+        'name': 'Germanium',
+        'symbol': 'Ge',
+        'number': 32,
+        'mass': 72.6308
+    },
+    'As': {
+        'name': 'Arsenic',
+        'symbol': 'As',
+        'number': 33,
+        'mass': 74.9215956
+    },
+    'Se': {
+        'name': 'Selenium',
+        'symbol': 'Se',
+        'number': 34,
+        'mass': 78.9718
+    },
+    'Br': {
+        'name': 'Bromine',
+        'symbol': 'Br',
+        'number': 35,
+        'mass': 79.9041
+    },
+    'Kr': {
+        'name': 'Krypton',
+        'symbol': 'Kr',
+        'number': 36,
+        'mass': 83.798
+    },
+    'Rb': {
+        'name': 'Rubidium',
+        'symbol': 'Rb',
+        'number': 37,
+        'mass': 85.46783
+    },
+    'Sr': {
+        'name': 'Strontium',
+        'symbol': 'Sr',
+        'number': 38,
+        'mass': 87.621
+    },
+    'Y': {
+        'name': 'Yttrium',
+        'symbol': 'Y',
+        'number': 39,
+        'mass': 88.905842
+    },
+    'Zr': {
+        'name': 'Zirconium',
+        'symbol': 'Zr',
+        'number': 40,
+        'mass': 91.2242
+    },
+    'Nb': {
+        'name': 'Niobium',
+        'symbol': 'Nb',
+        'number': 41,
+        'mass': 92.906372
+    },
+    'Mo': {
+        'name': 'Molybdenum',
+        'symbol': 'Mo',
+        'number': 42,
+        'mass': 95.951
+    },
+    'Tc': {
+        'name': 'Technetium',
+        'symbol': 'Tc',
+        'number': 43,
+        'mass': 98
+    },
+    'Ru': {
+        'name': 'Ruthenium',
+        'symbol': 'Ru',
+        'number': 44,
+        'mass': 101.072
+    },
+    'Rh': {
+        'name': 'Rhodium',
+        'symbol': 'Rh',
+        'number': 45,
+        'mass': 102.905502
     }
 };
