dca092a172683e65179a9e3b9fbf9743698ce1e5
Jared Kuemmerle <jared@drivecurrent.com>
2015-01-28 17:38:18 -0800

    initial commit

---
+++ chemword.js
@@ -0,0 +1,50 @@
+elements = {
+    'H': {
+        'name': 'Hydrogen',
+        'symbol': 'H',
+        'number': 1,
+        'mass': 1.0081
+    },
+    'He': {
+        'name': 'Helium',
+        'symbol': 'He',
+        'number': 2,
+        'mass': 4.0026022
+    },
+    'Li': {
+        'name': 'Lithium',
+        'symbol': 'Li',
+        'number': 3,
+        'mass': 6.941
+    },
+    'Be': {
+        'name': 'Beryllium',
+        'symbol': 'Be',
+        'number': 4,
+        'mass': 9.01218315
+    },
+    'B': {
+        'name': 'Boron',
+        'symbol': 'B',
+        'number': 5,
+        'mass': 10.811
+    },
+    'C': {
+        'name': 'Carbon',
+        'symbol': 'C',
+        'number': 6,
+        'mass': 12.0111
+    },
+    'N': {
+        'name': 'Nitrogen',
+        'symbol': 'N',
+        'number': 7,
+        'mass': 14.0071
+    },
+    'O': {
+        'name': 'Oxygen',
+        'symbol': 'O',
+        'number': 8,
+        'mass': 15.9994
+    }
+};
